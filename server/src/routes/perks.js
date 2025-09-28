d7caa152c82a4811e7a1d1a54fb4723a71f3601e
Mohamed Ayman <mohikel6@gmail.com>
2025-09-28 16:30:22 +0300

    finished the update perks logic

--- server/src/routes/perks.js
+++ server/src/routes/perks.js
@@ -1,14 +1,21 @@
-import { Router } from 'express';
-import { filterPerks, getPerk, createPerk, updatePerk, deletePerk, getAllPerks} from '../controllers/perkController.js';
+import { Router } from "express";
+import {
+  filterPerks,
+  getPerk,
+  createPerk,
+  updatePerk,
+  deletePerk,
+  getAllPerks,
+} from "../controllers/perkController.js";
 
 const router = Router();
 
-router.get('/', filterPerks);
-router.get('/all', getAllPerks);
-router.get('/:id', getPerk);
-router.post('/', createPerk);
+router.get("/", filterPerks);
+router.get("/all", getAllPerks);
+router.get("/:id", getPerk);
+router.post("/", createPerk);
 // put vs patch: put is for full updates, patch is for partial updates. they will both work,However it is just a convention.
-router.patch('/:id', updatePerk);
-router.delete('/:id', deletePerk);
+router.patch("/:id", updatePerk);
+router.delete("/:id", deletePerk);
 
 export default router;
